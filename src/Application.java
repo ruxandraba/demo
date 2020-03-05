fb93012bff6b80dd811fff60d8f03d2780ee22d3
Ruxandra Bucos <ruxandra.bucos@maxcode.net>
2020-03-05 14:41:47 +0200

    bar1

--- src/Application.java
+++ src/Application.java
@@ -2,5 +2,7 @@ public class Application {
 
   private static String name = "NUME";
   private static String nameMaster = "MASTER4";
+  private static String name1 = "BAR";
+  private static String nameMaster1 = "MASTER2";
 
 }
